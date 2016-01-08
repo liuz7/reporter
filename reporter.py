c630d6b5f0c28530dddaa30784fe1cbbc38c0c7b
zhiwen.liu <zhiwen.liu@dianping.com>
2016-01-08 18:52:38 +0800

    Initial commit for project reporter based on Flask

---
+++ reporter.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+from flask import Flask,render_template
+from flask.ext.bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config.from_object('config.default')
+#app.config.from_pyfile('config.py') # 从instance文件夹中加载配置
+#app.config.from_envvar('APP_CONFIG_FILE')
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+
+if __name__ == '__main__':
+    app.run()
